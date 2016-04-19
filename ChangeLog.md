da46de286058b28017626bcd4fe19032eb79c8bf
janhuang <384099566@qq.com>
2016-04-19 19:32:27 +0800

    update docs

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,7 +1,7 @@
 * 2.0.0 (2015-xx-xx)
 
 - 调整结构
-- 优化 Context 环境上下文对象
+- 移除 Context 对象
 - 移除 Invoker 对象
 - 删除无用代码
 - 调整 ServerHandlerInterface 接口
---
+++ examples/queue/server.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 16/4/15
+ * Time: 下午3:13
+ * Github: https://www.github.com/janhuang
+ * Coding: https://www.coding.net/janhuang
+ * SegmentFault: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ * Gmail: bboyjanhuang@gmail.com
+ * WebSite: http://www.janhuang.me
+ */
+
