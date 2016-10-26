517cf3add1e0a0e8ec9715d24bf14997cd2508e5
janhuang <bboyjanhuang@gmail.com>
2016-10-27 00:01:05 +0800

    add websocket

--- examples/websocket.php
+++ examples/websocket.php
@@ -11,6 +11,27 @@ use FastD\Swoole\WebSocket\WebSocketServer;
 
 include __DIR__ . '/../vendor/autoload.php';
 
+/*$server = new swoole_websocket_server("0.0.0.0", 9527);
+
+$server->on('open', function (swoole_websocket_server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";
+});
+
+$server->on('message', function (swoole_websocket_server $server, $frame) {
+    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    $server->push($frame->fd, "this is server");
+});
+
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+
+print_r($server);
+
+$server->start();
+
+die;*/
+
 class WebSocket extends WebSocketServer
 {
     /**
@@ -46,7 +67,8 @@ class WebSocket extends WebSocketServer
 }
 
 $webSocket = new WebSocket([
-    'host' => '0.0.0.0'
+    'host' => '0.0.0.0',
+    'port' => 9527
 ]);
 
 $webSocket->start();
