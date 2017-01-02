a67961b23c79ba2f043c9de4d2165fc007bbb687
JanHuang <bboyjanhuang@gmail.com>
2017-01-02 19:28:23 +0800

    add php sockets extension client

---
+++ examples/socket.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+
+use FastD\Swoole\Client\Socket;
+
+include __DIR__ . '/../vendor/autoload.php';
+
+$socket = new Socket('tcp://127.0.0.1:9527');
+
+$socket->connect(function (Socket $socket) {
+    $socket->send('hello world');
+})->receive(function ($data) {
+    echo ($data) . PHP_EOL;
+})->resolve();
+
