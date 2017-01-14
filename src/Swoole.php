94d7fd2d8ef3e2bd3ded19d6ad6fff7b043e9a0b
JanHuang <bboyjanhuang@gmail.com>
2017-01-14 20:51:17 +0800

    update swoole console application

--- src/Swoole.php
+++ src/Swoole.php
@@ -10,8 +10,13 @@
 namespace FastD\Swoole;
 
 
+use FastD\Swoole\Server\Http;
+use FastD\Swoole\Server\Tcp;
+use FastD\Swoole\Server\Udp;
+use FastD\Swoole\Server\WebSocket;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -19,11 +24,30 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Swoole extends Application
 {
+    const DEFAULT_COMMAND = 'server';
+
     public function __construct()
     {
         parent::__construct('swoole', Server::VERSION);
 
         $this->add(new ServerCommand());
+    }
+
+    /**
+     * @param InputInterface|null $input
+     * @param OutputInterface|null $output
+     * @return int
+     */
+    public function run(InputInterface $input = null, OutputInterface $output = null)
+    {
+        $argv = $_SERVER['argv'];
+
+        $script = array_shift($argv);
+
+        array_unshift($argv, static::DEFAULT_COMMAND);
+        array_unshift($argv, $script);
+
+        return parent::run(new ArgvInput($argv), $output);
     }
 }
 
@@ -32,7 +56,7 @@ class ServerCommand extends Command
     public function configure()
     {
         $this
-            ->setName('server')
+            ->setName(Swoole::DEFAULT_COMMAND)
             ->setHelp('This command allows you to create swoole server...')
             ->setDescription('Create new swoole server')
         ;
@@ -51,6 +75,47 @@ class ServerCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $address = '';
+        $server  = '';
+        switch ($input->getOption('type')) {
+            case 'http':
+                $address .= 'http://';
+                $server = Http::class;
+                break;
+            case 'tcp':
+                $address .= 'tcp://';
+                $server = Tcp::class;
+                break;
+            case 'udp':
+                $server = Udp::class;
+                $address .= 'udp://';
+                break;
+            case 'ws':
+                $server = WebSocket::class;
+                $address .= 'ws://';
+                break;
+            default:
+                throw new \LogicException('Not support server type ' . $input->getOption('type'));
+        }
 
+        $address .= $input->getOption('host') . ':' . $input->getOption('port');
+
+        $server = $server::createServer($input->getArgument('name'), $address);
+
+        switch ($input->getArgument('action')) {
+            case 'start':
+                $server->start();
+                break;
+            case 'stop':
+                break;
+            case 'status':
+                break;
+            case 'reload':
+                break;
+            case 'restart':
+                break;
+            default:
+                throw new \LogicException(sprintf('Not support action ' . $input->getArgument('action')));
+        }
     }
 }
